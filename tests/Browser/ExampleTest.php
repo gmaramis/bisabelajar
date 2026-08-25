d1d450feeb8964f3a5fffa94f23d62ad061267d2
toni <toniliu672@gmail.com>
2026-08-25 15:15:20 +0800

    test(browser): setup laravel dusk and headless chrome test suite

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     */
+    public function test_homepage_renders_in_browser(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->resize(360, 800)
+                ->visit('/')
+                ->assertSee('Documentation');
+        });
+    }
+}
